c40daf1460bc0695b47c73df49a16cc06cc22887
martin-fhnw <martin.schmitz@students.fhnw.ch>
2020-11-07 09:08:45 +0100

    cleanup updated - new cleaned sample created - columns from excel now excluded from dataset

--- Lending Club Group Work.R
+++ Lending Club Group Work.R
@@ -14,52 +14,127 @@ library(fiftystater)
 library(viridis)
 
 # setwd("C:/Users/phkem/Switch Drive/02_Studium/01_Master Olten/3. Semester/Data Science/Group Work")
-# df <- read.csv("regression_train_loan.csv",sep = ",", header = TRUE)
 # df_sample <- sample_n(df,10000)
-# write.csv(x = df_sample, file = "regression_train_loan_sample.csv")
 
-rm(list = ls())
-df_loan <- read.csv("regression_train_loan_sample.csv",sep = ",", header = TRUE)
+df_loan <- read.csv("../regression_train_loan.csv",sep = ",", header = TRUE)
 
-#View the Summary and get a glimpse of your data
+# Checking summary of the data and getting first insights
 summary(df_loan)
-glimpse(df_loan)
+glimpse(df_loan) 
 
-#View the Summary and get a glimpse of your data
+# Check for duplicates
 sum(duplicated(df_loan))
 
 
 ########## DATA CLEANING
+########## Based on insights above and further manual analysis 
+########## (summary, boxplot, histogram per column if this was possible) (see Excel Sheet)
 
-#  check the number of missing or NA values in the data set
+# Further check for NA values to be sure that we can delete them 
 colSums(is.na(df_loan))
-
-# Visualize the number of NA values
+# Visualization of the number of NA values
 options(repr.plot.width=6, repr.plot.height=8)
 missing_data <- df_loan %>% summarise_all(funs(sum(is.na(.))/n()))
 missing_data <- gather(missing_data, key = "variables", value = "percent_missing") 
-ggplot(missing_data, aes(x = reorder(variables, percent_missing), y = percent_missing)) +
-  geom_bar(stat = "identity", fill = "red", aes(color = I('white')), size = 0.1)+coord_flip()+ theme_few()
+ggplot(missing_data, 
+       aes(x = reorder(variables, percent_missing), 
+           y = percent_missing)) + geom_bar(stat = "identity", 
+                                            fill = "red", 
+                                            aes(color = I('white')), 
+                                            size = 0.1)+coord_flip() + theme_few()
 
 # get rid of columns having more than 10% of missing data
-loan_cleaned <- loan[, -which(colMeans(is.na(loan)) > 0.1)]
+loan_cleaned_I <- df_loan[, -which(colMeans(is.na(df_loan)) > 0.1)]
 
-# Removing the redundant & duplicate variables (to be compleated)
-loan_cleaned <- within(loan_cleaned, rm('member_id', 'id', 'url', 'emp_title', "title","zip_code"))
+# Check again visually  
+missing_data <- loan_cleaned_I %>% summarise_all(funs(sum(is.na(.))/n()))
+missing_data <- gather(missing_data, key = "variables", value = "percent_missing") 
+ggplot(missing_data, 
+       aes(x = reorder(variables, percent_missing), 
+           y = percent_missing)) + geom_bar(stat = "identity", 
+                                            fill = "red", 
+                                            aes(color = I('white')), 
+                                            size = 0.1)+coord_flip() + theme_few()
 
-# Remove variables which are not relevant or not present when the interest was decided (to be done!)
-loan_cleaned <- loan_cleaned[, -c(2,3,13,28,29,30,31,32,33,34,35,36,37,38,40,43)]
+# Removing all columns already identified as not usable in the Excel - some already removed by the previous operation
+df_loan_cleaned <- within(loan_cleaned_I, rm('X',
+                                          'url',
+                                          'id',
+                                          'member_id',
+                                          'installment',
+                                          'grade',
+                                          'emp_title',
+                                          'pymnt_plan',
+                                          'url',
+                                          'desc',
+                                          'title',
+                                          'delinq_2yrs',
+                                          'earliest_cr_line',
+                                          'pub_rec',
+                                          'last_pymnt_d',
+                                          'last_pymnt_amnt',
+                                          'next_pymnt_d',
+                                          'last_credit_pull_d',
+                                          'mths_since_last_major_derog',
+                                          'policy_code',
+                                          'open_acc_6m',
+                                          'open_il_6m',
+                                          'open_il_12m',
+                                          'open_il_24m',
+                                          'mths_since_rcnt_il',
+                                          'total_bal_il',
+                                          'il_util',
+                                          'open_rv_12m',
+                                          'open_rv_24m',
+                                          'max_bal_bc',
+                                          'all_util',
+                                          'total_rev_hi_lim',
+                                          'inq_fi',
+                                          'total_cu_tl',
+                                          'inq_last_12m'))
 
-# converting incorrect data types --> HAS TO BE CHEKED
-loan_cleaned$term <- as.factor(loan_cleaned$term)
-loan_cleaned$grade <- as.factor(loan_cleaned$grade)
-loan_cleaned$emp_length <- as.factor(loan_cleaned$emp_length)
-loan_cleaned$home_ownership <- as.factor(loan_cleaned$home_ownership)
-loan_cleaned$verification_status <- as.factor(loan_cleaned$verification_status)
-loan_cleaned$loan_status <- as.factor(loan_cleaned$loan_status)
-loan_cleaned$application_type <- as.factor(loan_cleaned$application_type)
-loan_cleaned$pymnt_plan <- as.factor(loan_cleaned$pymnt_plan)
-loan_cleaned$initial_list_status <- as.factor(loan_cleaned$initial_list_status)
-loan_cleaned$policy_code <- as.factor(loan_cleaned$policy_code)
+# Check again visually  
+missing_data <- df_loan_cleaned %>% summarise_all(funs(sum(is.na(.))/n()))
+missing_data <- gather(missing_data, key = "variables", value = "percent_missing") 
+ggplot(missing_data, 
+       aes(x = reorder(variables, percent_missing), 
+           y = percent_missing)) + geom_bar(stat = "identity", 
+                                            fill = "red", 
+                                            aes(color = I('white')), 
+                                            size = 0.1)+coord_flip() + theme_few()
 
-#
+write.csv(x = df_loan_cleaned, file = "regression_loan_cleaned.csv")
+df_sample <- sample_n(df_loan_cleaned,10000)
+write.csv(x = df_sample, file = "regression_train_loan_sample_cleaned.csv")
+
+## CLEANUP ## 
+# Must not be performed - but could improve performance!
+rm(list = ls())
+df_loan_sample <- read.csv("regression_train_loan_sample_cleaned.csv",sep = ",", header = TRUE)
+
+
+# converting incorrect data types to be able to work with them
+# factor variables
+df_loan_sample$term <- as.factor(df_loan_sample$term)
+# df_loan_sample$grade <- as.factor(df_loan_sample$grade) # not in the data set after our analysis
+df_loan_sample$emp_length <- as.factor(df_loan_sample$emp_length)
+df_loan_sample$home_ownership <- as.factor(df_loan_sample$home_ownership)
+df_loan_sample$verification_status <- as.factor(df_loan_sample$verification_status)
+df_loan_sample$loan_status <- as.factor(df_loan_sample$loan_status)
+df_loan_sample$application_type <- as.factor(df_loan_sample$application_type)
+df_loan_sample$initial_list_status <- as.factor(df_loan_sample$initial_list_status)
+# dates
+df_loan_sample$issue_d <- parse_date(as.character(df_loan_sample$issue_d), format =  "%b-%Y")
+
+# Analyze which data can be used for prediction and which might not help enough
+# selecting, counting and grouping by status in sample: 
+df_loan_sample.loan.distribution <- df_loan_sample %>% group_by(loan_status) %>% 
+                                    dplyr::summarise(count=n()) %>% mutate(pct=count/sum(count))
+
+ggplot(df_loan_sample.loan.distribution, 
+       aes(x=reorder(loan_status, pct), 
+           y=pct, colour=loan_status, fill=loan_status)) +
+  geom_bar(stat="identity",
+           aes(color = I('black')), 
+           size = 0.1) + coord_flip() + theme(legend.position = "none")+ xlab("Loan status") + ylab("Percentage in the dataset in the loan status")
+
